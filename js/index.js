e0463c6a60f646b2f7e91f4cf8bc91013b064151
Daniel Mateus <u1802833@unimilitar.edu.co>
2020-03-01 23:57:30 -0500

    [add] functions basic

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
 
 function IniciarVideo(){
     navigator.mediaDevices.getUserMedia({
@@ -13,10 +14,10 @@ function IniciarVideo(){
 }
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('../models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('../models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('../models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('../models')
+    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('./models')
 ])
 .then(
     () => {
@@ -28,3 +29,20 @@ Promise.all([
         console.log('error no cargaron los models');
     }
 )
+
+video.addEventListener('loadeddata', ()=>{
+    const ctx = canvas.getContext('2d');
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    const displaySize = { width: video.videoWidth, height: video.videoHeight }
+    faceapi.matchDimensions(canvas, displaySize);
+
+    setInterval(async ()=>{
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    }, 100);
+});
